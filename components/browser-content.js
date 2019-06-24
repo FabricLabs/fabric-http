bf664a529e3da4925fc529caafbbf9f2cc1880ef
Eric Martindale <eric@ericmartindale.com>
2019-06-24 01:56:05 -0700

    Add missing Browser Content component

---
+++ components/browser-content.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// const Component = require('./component');
+
+class BrowserContent {
+  constructor (settings = {}) {
+    // super(settings);
+    this.settings = Object.assign({
+      name: 'UnsafeBrowserContent',
+      handle: 'fabric-browser-content'
+    }, settings);
+    return this;
+  }
+
+  _getInnerHTML () {
+    return `<fabric-sample><h1>Waiting for data...</h1><p>The application is loading.  Unless you've enabled JavaScript or are using the Fabric Browser, this message should not remain visible for long.</p></fabric-sample>`;
+  }
+
+  render () {
+    return `<fabric-browser-content id="browser-content">${this._getInnerHTML()}</fabric-browser-content>`;
+  }
+}
+
+module.exports = BrowserContent;
