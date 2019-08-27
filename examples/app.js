1c0ecc8ec5eed83925fd6cd0bb138e7074738bf5
Eric Martindale <eric@ericmartindale.com>
2019-08-27 14:33:54 -0400

    Update App example

--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const Fabric = require('@fabric/core');
-const App = require('@fabric/http/types/app');
+const Script = require('@fabric/core/types/script');
+const App = require('../types/app');
 
 async function showPrompt (state) {
   let confirmed = confirm('Confirming this dialog will execute the next portion of the contract.  Canceling will abort.');
@@ -22,15 +22,26 @@ async function queryForUsername (state) {
 }
 
 async function main () {
+  let script = new Script({
+    history: [
+      { type: 'dispatch', data: { name: 'showPrompt' } },
+      { type: 'dispatch', data: { name: 'queryForUsername' } }
+    ]
+  });
   let app = new App({
     name: 'Example Serverless App',
     description: 'This example can be duplicated externally, '
   });
 
-  app._registerMethod0('showPrompt', showPrompt.bind(app));
-  app._registerMethod0('queryForUsername', queryForUsername.bind(app));
+  app._registerMethod('showPrompt', showPrompt.bind(app));
+  app._registerMethod('queryForUsername', queryForUsername.bind(app));
 
   await app.start();
+  await app.dispatch('showPrompt');
+  await app.dispatch('queryForUsername');
+
+  console.log('output app:', app);
+  console.log('output script:', script);
 }
 
 main();
