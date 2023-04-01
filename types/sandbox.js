899081bc6e902f855467ee9a3191bddd094b268d
Eric Martindale <eric@ericmartindale.com>
2023-04-01 05:14:15 -0400

    Import fomantic distributables, update various types

--- types/sandbox.js
+++ types/sandbox.js
@@ -23,6 +23,7 @@ class Sandbox extends Service {
 
     this.browser = null;
     this.chromium = null;
+    this.requests = [];
 
     this._state = {
       content: this.settings.state
@@ -37,12 +38,31 @@ class Sandbox extends Service {
     // Create browser instance
     this.chromium = await puppeteer.launch(this.settings.browser);
     this.browser = await this.chromium.newPage();
+
+    // Browser event handlers
     this.browser.on('console', (msg) => {
       this.emit('debug', `Sandbox console emitted: ${msg.text()}`)
     });
 
+    this.browser.on('request', (request) => {
+      this.requests.push(request);
+      const headers = request.headers();
+      headers['X-Fabric-Identity'] = '<not defined>';
+      request.continue({
+        headers
+      });
+    });
+
+    // Browser onfiguration
+    await this.browser.setRequestInterception(true);
+    await this.browser.setExtraHTTPHeaders({
+      'X-Fabric-Identity': '<not defined>'
+    });
+
+    // A E S T H E T I C S
     await this.browser.setViewport(this.settings.browser.viewport);
 
+    // Commit
     this._state.content.status = 'STARTED';
     this.commit();
 
